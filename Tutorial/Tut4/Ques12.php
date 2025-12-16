d6607af611ca08064d28028de3f2180092c05635
Shraddha Adhikari <shraddhaadhikari273@gmail.com>
2025-12-16 09:06:06 +0545

    Update Ques12.php

--- Tutorial/Tut4/Ques12.php
+++ Tutorial/Tut4/Ques12.php
@@ -2,6 +2,7 @@
 $data = json_decode(file_get_contents("products.json"), true);
 
 foreach ($data as $p) {
-    echo "Name: ".$p["name"]." - Price: ".$p["price"]."<br>";
+    echo "Name: ".$p["name"]." , Price: ".$p["price"]."<br>";
 }
+
 ?>
